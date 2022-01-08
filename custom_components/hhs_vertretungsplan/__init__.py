1de9a97c1ad8571ca417a7fddf70e9e206ff1109
kongo09 <kongo09@gmail.com>
2022-01-08 18:38:41 +0100

    time window

--- custom_components/hhs_vertretungsplan/__init__.py
+++ custom_components/hhs_vertretungsplan/__init__.py
@@ -80,22 +80,22 @@ class HHSDataUpdateCoordinator(DataUpdateCoordinator):
 
         """Only update in time window."""
         # todo: allow forced update
-        # try:
-        #     now = datetime.now().astimezone().replace(year=0, month=0, day=0, second=0, microsecond=0).isoformat()
-        #     start = datetime.strptime(POLLING_START, '%H:%M').isoformat()
-        #     _LOGGER.debug(f"start={start}")
-        #     end = datetime.strptime(POLLING_END, '%H:%M').isoformat()
-        #     _LOGGER.debug(f"end={end}")
-        #     if self.data is not None and (now < start or now > end):
-        #         _LOGGER.debug(f"Time is outside polling window, skipping update")
-        #         return self.data
-        # except (Exception) as error:
-        #     # in case something goes wrong with date/time parsing, we just update the data and continue
-        #     _LOGGER.error(f"Error occured with time parsing and comparison on update: {error}\n"
-        #                   f"Start and end times should be in format 'HH:MM'.\n"
-        #                   f"Configured are POLLING_START={POLLING_START} and POLLING_END={POLLING_END}\n"
-        #                   f"Please inform the maintainer of the integration.")
-        #     pass
+        try:
+            now = datetime.now().astimezone().replace(year=0, month=0, day=0, second=0, microsecond=0).isoformat()
+            start = datetime.strptime(POLLING_START, '%H:%M').isoformat()
+            _LOGGER.debug(f"start={start}")
+            end = datetime.strptime(POLLING_END, '%H:%M').isoformat()
+            _LOGGER.debug(f"end={end}")
+            if self.data is not None and (now < start or now > end):
+                _LOGGER.debug(f"Time is outside polling window, skipping update")
+                return self.data
+        except (Exception) as error:
+            # in case something goes wrong with date/time parsing, we just update the data and continue
+            _LOGGER.error(f"Error occured with time parsing and comparison on update: {error}\n"
+                          f"Start and end times should be in format 'HH:MM'.\n"
+                          f"Configured are POLLING_START={POLLING_START} and POLLING_END={POLLING_END}\n"
+                          f"Please inform the maintainer of the integration.")
+            pass
 
         try:
             """Ask the library to reload fresh data."""
